066a00476671c430ac35bdbe92661589c07b0f2d
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-06-23 10:07:00 +0600

    initial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dot: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        }
+    );
+});
