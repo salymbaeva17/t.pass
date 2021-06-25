2a8b0fd5636e54275b633dd1a5a9c895d6bf37c0
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-06-25 12:26:35 +0600

    add media.css

--- js/main.js
+++ js/main.js
@@ -4,9 +4,36 @@ $(document).ready(function(){
         loop: true,
         nav: false,
         dot: true,
+        dotsSpeed: 7,
         autoplay: true,
         autoplayTimeout: 4000,
         autoplayHoverPause: true,
         }
     );
 });
+
+
+// Also can pass in optional settings block
+
+
+AOS.init({
+    duration: 1200,
+});
+
+
+// Show menu
+
+const burgerMenu = document.querySelector('.mobile__menu')
+const topMenu = document.querySelector('.header__menu')
+
+burgerMenu.addEventListener('click', () => {
+    topMenu.classList.toggle('show')
+})
+
+
+// Hide menu when click nav links/btn
+
+const menuLinks = document.querySelectorAll('.header__link')
+menuLinks.forEach(item => item.addEventListener('click', () => {
+    topMenu.classList.remove('show')
+}))
